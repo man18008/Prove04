4f047762cf073647ac1030ed0de81020ffe7e94f
tomtomm <bigdtibbs@gmail.com>
2020-10-08 20:18:45 -0600

    Oh please don't let me mess this up ...

--- src/meganmanwaring/Main.java
+++ src/meganmanwaring/Main.java
@@ -3,6 +3,7 @@ package meganmanwaring;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello, my name is Megan");
+        System.out.print("Hello, my name is Megan\n") ;
+        System.out.println("Hello Megan, my name is Daren.");
     }
 }
