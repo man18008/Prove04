3e7e5b6af01402e6c33d2840b505e873a908cef9
blbaileyiii <blbaileyiii.naa@gmail.com>
2020-10-09 15:34:21 -0700

    Lee's Changes

--- src/meganmanwaring/Main.java
+++ src/meganmanwaring/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Hello, my name is Megan\n") ;
         System.out.println("Hello Megan, my name is Daren.");
+        System.out.println("Hello Megan, my name is Lee.");
     }
 }
